3592fc25e6743685437be705a998f4a063933e12
Akash Parmar <akashparmar1520@gmail.com>
2024-12-15 19:59:45 +0530

    [ADD]  : test case to inspect accepted arguments by add method

--- Calculator.test.ts
+++ Calculator.test.ts
@@ -7,3 +7,9 @@ test("Calculator class exists", () => {
 test("Calculator class has add method", () => {
   expect(new Calculator.Calculator().add).toBeDefined();
 })
+
+test('Add method accepts a string as an argument', () => {
+  const calculator = new Calculator.Calculator();
+  expect(() => calculator.add('')).not.toThrow();
+  expect(() => calculator.add('1,2')).not.toThrow();
+});
