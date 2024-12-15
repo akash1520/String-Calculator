5fcc3e01342391da6fe66e37e45f558e0a07e033
Akash Parmar <akashparmar1520@gmail.com>
2024-12-15 14:49:42 +0530

    [ADD] : added a test case for add method
    Added a test case to check if 'add' method exists
    on Calculator class

--- Calculator.test.ts
+++ Calculator.test.ts
@@ -3,3 +3,7 @@ import * as Calculator from "./index";
 test("Calculator class exists", () => {
   expect(Calculator.Calculator).toBeDefined();
 });
+
+test("Calculator class has add method", () => {
+  expect(new Calculator.Calculator().add).toBeDefined();
+})
