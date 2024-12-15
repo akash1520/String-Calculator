78e024a29b5f9ef8e62ff05891759a5e8baf6f66
Akash Parmar <akashparmar1520@gmail.com>
2024-12-16 00:46:11 +0530

    [IMP] : custom delimiter support for add method

--- Calculator.ts
+++ Calculator.ts
@@ -1,5 +1,12 @@
 export class Calculator {
     add(numbers: string): number {
+        if (numbers.startsWith('//')) {
+            const [delimiterLine, numbersString] = numbers.split('\n');
+            const delimiter = delimiterLine.slice(2);
+
+            const numbersArray = numbersString.split(delimiter).map(Number);
+            return numbersArray.reduce((sum, num) => sum + num, 0);
+        }
         const numbersArray = numbers.replace(/\n/g, ',').split(',').map(Number);
         return numbersArray.reduce((sum, num) => sum + num, 0);
     }
