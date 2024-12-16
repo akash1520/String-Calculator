97446cb4a6d217101730d35f1acc2b5d80f3d2a6
Akash Parmar <akashparmar1520@gmail.com>
2024-12-16 12:47:21 +0530

    [REF]: test cases

--- Calculator.test.ts
+++ Calculator.test.ts
@@ -15,7 +15,12 @@ describe('Calculator', () => {
         expect(calculator.add).toBeDefined();
     });
 
+    test("Calculator class has getCalledCount method", () => {
+        expect(calculator.getCalledCount).toBeDefined();
+    });
+
     describe('Add method', () => {
+
         test('Accepts a string as an argument', () => {
             expect(() => calculator.add('')).not.toThrow();
             expect(() => calculator.add('1,2')).not.toThrow();
@@ -28,9 +33,10 @@ describe('Calculator', () => {
 
         test('Accepts more than two numbers', () => {
             expect(calculator.add('1,2,3')).toBe(6);
-        })
+        });
 
         describe('Delimiter support', () => {
+
             test('Supports custom delimiter specified at the start', () => {
                 expect(calculator.add('//;\n1;2')).toBe(3);
                 expect(calculator.add('//|\n1|2|3')).toBe(6);
@@ -41,7 +47,7 @@ describe('Calculator', () => {
                 expect(calculator.add('//[----]\n1----2----3')).toBe(6);
                 expect(calculator.add('//[##]\n1##2##3')).toBe(6);
             });
-            
+
             test('Supports new line as delimiter', () => {
                 expect(calculator.add('1\n2,3')).toBe(6);
             });
@@ -51,37 +57,33 @@ describe('Calculator', () => {
                 expect(calculator.add('//[***][%%%]\n1***2%%%3')).toBe(6);
                 expect(calculator.add('//[##][$$]\n1##2$$3')).toBe(6);
             });
-            
         });
-        
+
         test('Throws an error for negative numbers', () => {
             expect(() => calculator.add('-1,2')).toThrow('negative numbers not allowed -1');
         });
 
         test('Throws an error with multiple negative numbers', () => {
             expect(() => calculator.add('-1,-2,3')).toThrow('negative numbers not allowed -1,-2');
-        })
+        });
 
         test('Number bigger than 1000 should be ignored', () => {
             expect(calculator.add('//;\n1001;2;3')).toBe(5);
             expect(calculator.add('//;\n2005;2;3')).toBe(5);
-        })
+        });
     });
 
     describe('getCalledCount method', () => {
-        test("Calculator class has getCalledCount method", () => {
-            expect(calculator.getCalledCount).toBeDefined();
-        });
 
         test('getCalledCount returns number of times add was called', () => {
             expect(calculator.getCalledCount()).toBe(0);
-    
+
             calculator.add('1,2');
             expect(calculator.getCalledCount()).toBe(1);
-    
+
             calculator.add('3,4');
             expect(calculator.getCalledCount()).toBe(2);
-    
+
             calculator.add('');
             expect(calculator.getCalledCount()).toBe(3);
         });
