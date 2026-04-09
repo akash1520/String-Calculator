ad26b2dc860965779a1c7d1c10145d3d17fa8dfa
akash1520 <akashparmar1520@gmail.com>
2026-04-09 13:36:30 +0530

    [REF]: clean code compliance

--- Calculator.ts
+++ Calculator.ts
@@ -1,31 +1,49 @@
 export class Calculator {
     private callCount: number = 0;
-    
+
     add(numbers: string): number {
         this.callCount++;
-        if (numbers.startsWith('//')) {
-            const [delimiterLine, numbersString] = numbers.split('\n');
-            const delimiterMatches = delimiterLine.match(/\[(.*?)\]/g);
-            let delimiter: string;
+        const numberTokens = this.parseNumberTokens(numbers);
+        return this.sumNumbers(numberTokens);
+    }
 
-            if (delimiterMatches) {
-                delimiter = delimiterMatches.map(match => 
-                    match.replace(/[\[\]]/g, '')
-                ).map(delim => delim.replace(/[.*+?^=!:${}()|\[\]\/\\]/g, "\\$&"))
-                 .join('|');
-            } else {
-                delimiter = delimiterLine.slice(2);
-            }
-
-            const numbersArray = numbersString.split(new RegExp(delimiter));
-            return this.sumNumbers(numbersArray);
+    private parseNumberTokens(numbers: string): string[] {
+        if (this.hasCustomDelimiter(numbers)) {
+            return this.parseCustomDelimitedTokens(numbers);
         }
-        const numbersArray = numbers.replace(/\n/g, ',').split(',').map(Number);
-        return this.sumNumbers(numbers.replace(/\n/g, ',').split(','));
+
+        return this.normalizeDefaultDelimiters(numbers).split(',');
+    }
+
+    private hasCustomDelimiter(numbers: string): boolean {
+        return numbers.startsWith('//');
+    }
+
+    private parseCustomDelimitedTokens(numbers: string): string[] {
+        const [delimiterLine, numbersString] = numbers.split('\n');
+        const delimiterPattern = this.buildDelimiterPattern(delimiterLine);
+        return numbersString.split(new RegExp(delimiterPattern));
+    }
+
+    private buildDelimiterPattern(delimiterLine: string): string {
+        const delimiterMatches = delimiterLine.match(/\[(.*?)\]/g);
+
+        if (delimiterMatches) {
+            return delimiterMatches
+                .map(match => match.replace(/[\[\]]/g, ''))
+                .map(delim => delim.replace(/[.*+?^=!:${}()|\[\]\/\\]/g, "\\$&"))
+                .join('|');
+        }
+
+        return delimiterLine.slice(2);
+    }
+
+    private normalizeDefaultDelimiters(numbers: string): string {
+        return numbers.replace(/\n/g, ',');
     }
 
     private sumNumbers(numberStrings: string[]): number {
-        const numbersArray = numberStrings.map(Number).filter(num => num <= 1000);;
+        const numbersArray = numberStrings.map(Number).filter(num => num <= 1000);
         const negatives = numbersArray.filter(num => num < 0);
         if (negatives.length > 0) {
             throw new Error(`negative numbers not allowed ${negatives.join(',')}`);
